a89e97fd6a769be14107ef0d5d259e8f2532807e
Andrey Semenov <101043571+Andrew-Sem@users.noreply.github.com>
2024-04-11 19:15:26 +0300

    now moving calendar while select another month

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,18 +1,19 @@
-import {UserButton} from "@clerk/nextjs";
-import {Calendar} from "./_components/calendar";
-import {ModeToggle} from "./_components/mode-toggle";
+import { UserButton } from "@clerk/nextjs";
+import { Calendar } from "./_components/calendar";
+import { ModeToggle } from "./_components/mode-toggle";
 
 export default async function Home() {
-    return (
-        <main className="min-h-screen flex flex-col relative">
-            <div className="fixed top-5 left-5">
-                <UserButton/>
-            </div>
-            <Calendar/>
-            <div className="fixed left-5 bottom-5">
-                <ModeToggle/>
-            </div>
-
-        </main>
-    );
+  return (
+    <main className="relative flex min-h-screen flex-col">
+      <div className="fixed left-5 top-5">
+        <UserButton />
+      </div>
+      <div className="mt-40 flex flex-col items-center">
+        <Calendar />
+      </div>
+      <div className="fixed bottom-5 left-5">
+        <ModeToggle />
+      </div>
+    </main>
+  );
 }
