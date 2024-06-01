7c36b976394c08388681f6312f41761617c599ae
Andrey Semenov <101043571+Andrew-Sem@users.noreply.github.com>
2024-06-01 15:48:34 +0300

    display articles

--- src/app/Material/page.tsx
+++ src/app/Material/page.tsx
@@ -1,25 +1,12 @@
 import { api } from "@/trpc/server";
+import { ArticleList } from "../_components/article-list";
 
 export default async function MaterialPage() {
   const articles = await api.article.getArticles();
+
   return (
-    <div>
-      <table>
-        <thead>
-          <tr>
-            <th>Title</th>
-            <th>Content</th>
-          </tr>
-        </thead>
-        <tbody>
-          {articles.map((article) => (
-            <tr key={article.id}>
-              <td style={{ fontSize: "12px" }}>{article.title}</td>
-              <td>{article.content}</td>
-            </tr>
-          ))}
-        </tbody>
-      </table>
+    <div className="flex">
+      <ArticleList articles={articles} />
     </div>
   );
 }
---
+++ src/app/_components/article-list.tsx
@@ -0,0 +1,38 @@
+"use client";
+import { type Article } from "@prisma/client";
+import { useState } from "react";
+
+export const ArticleList = ({ articles }: { articles: Article[] }) => {
+  const [activeArticle, setActiveArticle] = useState<null | Article>(null);
+
+  return (
+    <div className="flex h-[calc(100vh-100px)] w-full">
+      <div
+        className="w-1/4 overflow-y-auto border-r"
+        style={{ maxHeight: "100vh" }}
+      >
+        <ul>
+          {articles.map((article) => (
+            <li
+              key={article.id}
+              className="cursor-pointer p-2 hover:bg-secondary"
+              onClick={() => setActiveArticle(article)}
+            >
+              {article.title}
+            </li>
+          ))}
+        </ul>
+      </div>
+      <div className="w-3/4 overflow-y-auto p-4" style={{ maxHeight: "100vh" }}>
+        {activeArticle ? (
+          <div>
+            <h1 className="mb-2 text-xl font-bold">{activeArticle.title}</h1>
+            <p>{activeArticle.content}</p>
+          </div>
+        ) : (
+          <p>Select an article to read</p>
+        )}
+      </div>
+    </div>
+  );
+};
