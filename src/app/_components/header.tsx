0d076399a3b9efbfb5c12d5c4ab5c6130a421ca5
Andrey Semenov <101043571+Andrew-Sem@users.noreply.github.com>
2024-06-01 15:05:30 +0300

    bettter ui

--- src/app/_components/header.tsx
+++ src/app/_components/header.tsx
@@ -1,23 +1,24 @@
 import { UserButton } from "@clerk/nextjs";
 import Link from "next/link";
 import { ModeToggle } from "./mode-toggle";
+import Image from "next/image";
 
 export const Header = () => {
   return (
     <header className="border-bottom container sticky flex justify-between bg-background/20 py-2 backdrop-blur-sm">
-      <div className="image-container">
-        <a href="/">
-          <img src="/Logo.png" width="60" />
-        </a>
-      </div>
-      <div>
-        <Link href="/">Калькулятор</Link>
-      </div>
-      <div>
-        <Link href="/Material">Полезные материалы</Link>
-      </div>
-      <div>
-        <Link href="/uguide">Руководство пользователя</Link>
+      <a href="/">
+        <Image src="/Logo.png" width={60} height={60} alt="logo" />
+      </a>
+      <div className="flex items-center space-x-10">
+        <div>
+          <Link href="/">Калькулятор</Link>
+        </div>
+        <div>
+          <Link href="/material">Полезные материалы</Link>
+        </div>
+        <div>
+          <Link href="/uguide">Руководство пользователя</Link>
+        </div>
       </div>
       <div className="flex items-center gap-x-4">
         <ModeToggle />
